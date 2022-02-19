5bf316ecdbe97d19e363f8501f85bf59dda6df1e
irinaeris <irina.vasiu.eris@gmail.com>
2022-02-19 12:55:59 +0200

    exercitiul 3

--- 04 week/tema.js
+++ 04 week/tema.js
@@ -1,9 +1,10 @@
 /*var array = [ 1, 2, 3, 4, 56, 232, 45, 12, 4, 6, 8, 8,8, 1, 1] 
-Using control structures (if, for, while), do the following:
+Using control structures (if, for, while), do the following:*/
 
-1. display in the console the numbers from 1 to 20 */
+//1. display in the console the numbers from 1 to 20 
 
 
+/*
 var array = [ 1, 2, 3, 4, 56, 232, 45, 12, 4, 6, 8, 8,8, 1, 1];
 
 var lungime=array.length;
@@ -12,6 +13,38 @@ for (i=0; i< lungime; i=i+1) {
     if (array[i] < 20) {
         console.log(array[i])
     }
-   
 }
+*/
+
+//2. display in the console the odd numbers from 1 to 20
+
+/*
+var array = [ 1, 2, 3, 4, 56, 232, 45, 12, 4, 6, 8, 8,8, 1, 1];
+
+var lungime=array.length;
+
+for (i=0; i< lungime; i=i+1) {
+
+    if (array[i] < 20) {
+
+        if (array[i]%2===0)
+
+        console.log(array[i])
+    }
+}
+*/
+
+//3. compute the sum of the elements of an array and display it in the console
+
+var array = [1, 2, 3, 4, 56, 232, 45, 12, 4, 6, 8, 8,8, 1, 1];
+var result=0;
+
+for (let i=0; i < array.length; i++) {
+       
+        result += array[i];
+
+
+    }
+
+    console.log(result)
 
