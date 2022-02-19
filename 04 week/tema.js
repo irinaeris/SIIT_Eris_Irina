bc7ff807a65faddc762c825044351d84990ea932
irinaeris <irina.vasiu.eris@gmail.com>
2022-02-19 13:32:42 +0200

    exercitiul 5

--- 04 week/tema.js
+++ 04 week/tema.js
@@ -36,6 +36,7 @@ for (i=0; i< lungime; i=i+1) {
 
 //3. compute the sum of the elements of an array and display it in the console
 
+/*
 var array = [1, 2, 3, 4, 56, 232, 45, 12, 4, 6, 8, 8,8, 1, 1];
 var result=0;
 
@@ -47,4 +48,48 @@ for (let i=0; i < array.length; i++) {
     }
 
     console.log(result)
+*/
+
+//4. compute the maximum of the elements of an array and display it in the console
+
+/*
+var array = [1, 2, 3, 4, 56, 232, 45, 12, 4, 6, 8, 8,8, 1, 1];
+var max=array[0];
+
+
+
+for (let i=0; i < array.length; i++) {
+       
+
+        if (array[i] > max){
+        
+            max=array[i];
+            
+        }
+    }
+console.log(max)
+*/
+
+//5. compute how many times a certain element appears in an array - tips: use object, var element = 2
+
+
+
+
+var arr = [1, 2, 3, 4, 56, 232, 45, 12, 4, 6, 8, 8,8, 1, 1];
+const countNumber=(arr, value) => {
+
+    let count=0;
+    for (let i=0; i < arr.length; i++) {
+
+        if (arr[i] ===value){
+
+            count++
+            continue;
+        }
+    }
+    return count;
+}
+
+console.log(countNumber(arr, 8));
+
 
