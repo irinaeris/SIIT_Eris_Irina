0e05adee4f8db56457ff23b78f48e838d3f56b23
irinaeris <irina.vasiu.eris@gmail.com>
2022-02-20 20:16:06 +0200

    intermediate medium

---
+++ 04_week_tema_optionala/medium.js
@@ -0,0 +1,16 @@
+// 1. https://edabit.com/challenge/4gzDuDkompAqujpRi
+ // Create a function that takes a number as an argument. Add up all the numbers from 1 to the number 
+ // you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+ /*
+ function addUp(num) {
+ var  sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+	return sum;
+}
+*/
+
+// 2. https://edabit.com/challenge/Q3n42rEWanZSTmsJm
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
