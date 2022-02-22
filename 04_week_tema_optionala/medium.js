1cb50995182a135b23b985d41ff01a06d60331dd
irinaeris <irina.vasiu.eris@gmail.com>
2022-02-22 12:56:49 +0200

    3rd exercise

--- 04_week_tema_optionala/medium.js
+++ 04_week_tema_optionala/medium.js
@@ -14,3 +14,39 @@
 
 // 2. https://edabit.com/challenge/Q3n42rEWanZSTmsJm
 // Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+/*
+function minMax(arr) {
+  var min = arr[0];
+  var max = arr[0];
+	var returnNum=[]
+  for (var i = 1; i < arr.length; i++) {
+  			if (arr[i] < min) {
+    min = arr[i];
+				}
+	
+				 if (arr[i] > max) {
+    max = arr[i];
+  			}
+	}
+	  returnNum.push(min);
+    returnNum.push(max);
+  
+  return returnNum;
+  */
+
+  // 3. https://edabit.com/challenge/Mc6Xi4PRw7fDzeMDB
+  // Create a function that takes a string and returns a string in which each character is repeated once.
+
+  /*
+  function doubleChar(str) {
+	 var new = "";
+	 for (var i=1; i< str.length; i++){
+		 
+		 new = str[i]+ str[i];
+	 }
+	return new;
+}*/
+
+// 4. https://edabit.com/challenge/Mo6Coy4PpTbZgtDKd
+// A set is a collection of unique items. A set can be formed from an array by removing all duplicate items.
