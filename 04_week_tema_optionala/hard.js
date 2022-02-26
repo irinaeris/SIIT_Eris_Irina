3251535a6a99ff38a719aaffcb36ad65c8137870
irinaeris <irina.vasiu.eris@gmail.com>
2022-02-26 17:03:42 +0100

    add home/ functions files

---
+++ 04_week_tema_optionala/hard.js
@@ -0,0 +1,6 @@
+// 1. https://edabit.com/challenge/c23dFfNiKbnguSQtq
+
+// You're given a string of words. You need to find the word "Nemo", and 
+//return a string like this: "I found Nemo at [the order of the word you find Nemo]!".
+
+//If you can't find Nemo, return "I can't find Nemo :(".
