1244f78b030213cd32f26a2dbc9f59041c0dd019
irinaeris <irina.vasiu.eris@gmail.com>
2022-03-15 14:29:17 +0200

    start and stop buttons

---
+++ 07_week/app.js
@@ -0,0 +1,63 @@
+const hourHtml = document.getElementById("hour");
+const minuteHtml = document.getElementById("minute");
+const secundeHtml = document.getElementById("secunde");
+
+console.log(hourHtml);
+console.log(minuteHtml);
+console.log(secundeHtml);
+
+function addZero(value) {
+  if (value < 10) {
+    return "0" + value;
+  }
+  return value;
+}
+
+let startBtn = document.getElementById("startButton");
+let resetButton=document.getElementById("resetButton")
+
+
+let secunde = 0;
+let minute = 0;
+let hour = 0;
+let intervalId;
+
+
+
+startBtn.addEventListener("click", function () {
+   
+  intervalId = setInterval(function () {
+
+    secunde = secunde + 1;
+
+    if (secunde == 60) {
+      minute = minute + 1;
+      secunde = 0;
+    }
+
+    if (minute == 60) {
+      hour = hour + 1;
+      minute = 0;
+    }
+
+    secundeHtml.innerHTML = addZero(secunde);
+    minuteHtml.innerHTML = addZero(minute);
+    hourHtml.innerHTML = addZero(hour);
+  }, 1000);
+
+});
+
+function myStop(){
+    clearInterval(intervalId);
+    
+}
+
+resetButton.addEventListener("click", function(){
+    myStop();
+    hourHtml.innerHTML="00";
+    minuteHtml.innerHTML="00";
+    secundeHtml.innerHTML="00";
+
+})
+
+
