fd0764c169e5c15c10e8cd5489a3be07c2d6fd46
irinaeris <irina.vasiu.eris@gmail.com>
2022-03-15 23:49:52 +0200

    save button

--- 07_week/app.js
+++ 07_week/app.js
@@ -14,13 +14,15 @@ function addZero(value) {
 }
 
 let startBtn = document.getElementById("startButton");
-let resetButton=document.getElementById("resetButton")
+let resetButton=document.getElementById("resetButton");
+let saveButton = document.getElementById("saveButton");
 
 
 let secunde = 0;
 let minute = 0;
 let hour = 0;
 let intervalId;
+
 
 
 
@@ -47,10 +49,11 @@ startBtn.addEventListener("click", function () {
 
 });
 
-function myStop(){
+  function myStop(){
     clearInterval(intervalId);
     
 }
+
 
 resetButton.addEventListener("click", function(){
 
@@ -59,6 +62,21 @@ resetButton.addEventListener("click", function(){
     secundeHtml.innerHTML="00";
     
 })
+
+  saveButton.onclick= function (){
+
+    const valuesHour = document.getElementById("hour");
+    const clone = valuesHour.cloneNode(true)
+    document.body.appendChild(clone)
+
+    const  valuesMinutes = document.getElementById("minute")
+    const clone2= valuesMinutes.cloneNode(true)
+    document.body.appendChild(clone2)
+
+    const  valuesSeconds = document.getElementById("secunde")
+    const clone3=valuesSeconds.cloneNode(true)
+    document.body.appendChild(clone3)
+  }
 
 
 
