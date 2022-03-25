7a717f33f1260aba9ed0f3780859906ebd4b90eb
irinaeris <irina.vasiu.eris@gmail.com>
2022-03-25 22:32:16 +0200

    fetch functions for all 3 animals + dropdown

---
+++ 08_week_tema_optionala/dog.js
@@ -0,0 +1,71 @@
+
+
+/*function getRandomDogs(){
+
+ url = "https://random.dog/woof.json"
+
+fetch(url) 
+    .then(function(response) {
+      //console.log(response);
+      return response.json();
+    })
+  .then(function (data) {
+   //console.log(data);
+    displayImageDog(data.url)
+   })
+
+}
+
+getRandomDogs();
+
+function displayImageDog(image){
+  document.getElementById("image").src = image
+
+}*/
+
+
+function getRandomCats(){
+
+    url = "https://aws.random.cat/meow"
+   
+   fetch(url) 
+       .then(function(response) {
+         //console.log(response);
+         return response.json();
+       })
+     .then(function (data) {
+      //console.log(data);
+       displayImageCat(data.file)
+      })
+   
+   }
+   
+   getRandomCats();
+   
+   function displayImageCat(image){
+     document.getElementById("image").src = image
+   
+   }
+
+   function getRandomFox(){
+
+    url = "https://randomfox.ca/floof/"
+   
+   fetch(url) 
+       .then(function(response) {
+         //console.log(response);
+         return response.json();
+       })
+     .then(function (data) {
+      //console.log(data);
+       displayImageFox(data.image)
+      })
+   
+   }
+   
+   getRandomFox();
+   
+   function displayImageFox(imageFox){
+     document.getElementById("image").src = imageFox
+   
+   }
