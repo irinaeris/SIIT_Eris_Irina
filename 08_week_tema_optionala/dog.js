54c57f6cf909ca3c10bc834053dfa13b5ad2ba99
irinaeris <irina.vasiu.eris@gmail.com>
2022-04-03 14:55:00 +0300

    added the get request upon dropdown selection

--- 08_week_tema_optionala/dog.js
+++ 08_week_tema_optionala/dog.js
@@ -2,142 +2,58 @@ let output;
 function getSelectedDropdownValue() {
   selectElement = document.getElementById("select1");
   let output = selectElement.value;
-  console.log(output);
   document.querySelector(".output").textContent = output;
 
-
-}
-
-function getRandomImage() {
   if (output === "Dog") {
-    function getRandomDogs() {
-      url = "https://random.dog/woof.json";
+    //function getRandomDogs() {
+    url = "https://random.dog/woof.json";
 
-      fetch(url)
-        .then(function (response) {
-          //console.log(response);
-          return response.json();
-        })
-        .then(function (data) {
-          //console.log(data);
-          displayImageDog(data.url);
-        });
-    }
-
-    getRandomDogs();
+    fetch(url)
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        displayImageDog(data.url);
+      });
 
     function displayImageDog(image) {
       document.getElementById("image").src = image;
     }
   } else if (output === "Cat") {
-    function getRandomCats() {
-      url = "https://aws.random.cat/meow";
+    //function getRandomCats() {
+    url = "https://aws.random.cat/meow";
 
-      fetch(url)
-        .then(function (response) {
-          //console.log(response);
-          return response.json();
-        })
-        .then(function (data) {
-          //console.log(data);
-          displayImageCat(data.file);
-        });
-    }
-
-    getRandomCats();
+    fetch(url)
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        displayImageCat(data.file);
+      });
 
     function displayImageCat(image) {
       document.getElementById("image").src = image;
     }
   } else {
-    function getRandomFox() {
-      url = "https://randomfox.ca/floof/";
+    //function getRandomFox() {
+    url = "https://randomfox.ca/floof/";
 
-      fetch(url)
-        .then(function (response) {
-          //console.log(response);
-          return response.json();
-        })
-        .then(function (data) {
-          //console.log(data);
-          displayImageFox(data.image);
-        });
-    }
+    fetch(url)
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        displayImageFox(data.image);
+      });
 
-    getRandomFox();
-
-    function displayImageFox(imageFox) {
-      document.getElementById("image").src = imageFox;
+    function displayImageFox(image) {
+      document.getElementById("image").src = image;
     }
   }
 }
-
-/*function getRandomDogs(){
-
- url = "https://random.dog/woof.json"
-
-fetch(url) 
-    .then(function(response) {
-      //console.log(response);
-      return response.json();
-    })
-  .then(function (data) {
-   //console.log(data);
-    displayImageDog(data.url)
-   })
-
-}
-
-getRandomDogs();
-
-function displayImageDog(image){
-  document.getElementById("image").src = image
-
-}
-
-
-function getRandomCats(){
-
-    url = "https://aws.random.cat/meow"
-   
-   fetch(url) 
-       .then(function(response) {
-         //console.log(response);
-         return response.json();
-       })
-     .then(function (data) {
-      //console.log(data);
-       displayImageCat(data.file)
-      })
-   
-   }
-   
-   getRandomCats();
-   
-   function displayImageCat(image){
-     document.getElementById("image").src = image
-   
-   }
-
-   function getRandomFox(){
-
-    url = "https://randomfox.ca/floof/"
-   
-   fetch(url) 
-       .then(function(response) {
-         //console.log(response);
-         return response.json();
-       })
-     .then(function (data) {
-      //console.log(data);
-       displayImageFox(data.image)
-      })
-   
-   }
-   
-   getRandomFox();
-   
-   function displayImageFox(imageFox){
-     document.getElementById("image").src = imageFox
-   
-   }*/
